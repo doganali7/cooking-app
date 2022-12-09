70ed5a486253962069f66e12da18650bdbd2a296
Ali Dogan <alidogn98@gmail.com>
2022-12-09 15:41:40 +0200

    useHistory added

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import "./Create.css";
 
@@ -10,6 +11,7 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
+  const history = useHistory;
 
   const { postData, data, error } = useFetch(
     "http://localhost:3002/recipes",
@@ -36,6 +38,9 @@ export default function Create() {
     setNewIngredient("");
     ingredientInput.current.focus();
   };
+
+  // redeirct user when we get data responce
+  useEffect(() => {}, [data]);
 
   return (
     <div className="create">
