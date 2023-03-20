d490f8324def9918bdfefd1aa7a8387be98520d5
doganali7 <alidogn98@gmail.com>
2023-03-21 01:39:57 +0200

    useTheme and ThemeContext created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 import React from "react";
 import "./Navbar.css";
 import SearchBar from "./SearchBar";
+import { useTheme } from "../hooks/useTheme";
 
 export default function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ backgroundColor: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking App</h1>
