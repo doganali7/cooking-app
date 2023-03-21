b261087d61cfc1c5760b923f500586d643a2d2c0
doganali7 <alidogn98@gmail.com>
2023-03-21 02:34:45 +0200

    changing color with useReducer when user clicks on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ import SearchBar from "./SearchBar";
 import { useTheme } from "../hooks/useTheme";
 
 export default function Navbar() {
-  const { color } = useTheme();
+  const { color, changeColor } = useTheme();
 
   return (
     <div className="navbar" style={{ backgroundColor: color }}>
-      <nav>
+      <nav onClick={() => changeColor("black")}>
         <Link to="/" className="brand">
           <h1>Cooking App</h1>
         </Link>
